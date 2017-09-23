1e2a24930cc48e9993b18b74d893f118667862a3
student8 <student8@itpark.ru>
2017-09-23 20:50:41 +0300

    start task1 and task2

---
+++ Arrays/src/ru/itpark/Main.java
@@ -0,0 +1,23 @@
+package ru.itpark;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int[] array = new int[N];
+        for (int i = 0; i < N; i++) {
+            array[i] = scanner.nextInt();
+        }
+        for (int i = 0; i < N; i++) {
+            if (array[i] % 2 == 0) {
+                array[i] = 0;
+            }
+        }
+        for (int i = N; i > 1; i--) {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
