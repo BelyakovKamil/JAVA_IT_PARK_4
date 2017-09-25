a1fd472e7229b582b85586073dc5b58a646c7270
BelyakovKamil <belyakov.kamil@mail.ru>
2017-09-25 22:52:56 +0300

    done homework 1

--- Arrays/src/ru/itpark/Main.java
+++ Arrays/src/ru/itpark/Main.java
@@ -1,23 +1,49 @@
-package ru.itpark;
-
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        int[] array = new int[N];
-        for (int i = 0; i < N; i++) {
-            array[i] = scanner.nextInt();
-        }
-        for (int i = 0; i < N; i++) {
-            if (array[i] % 2 == 0) {
-                array[i] = 0;
+
+        System.out.println("Введите число A:");
+        System.out.println("Число A = ");
+
+        int chislo_A = scanner.nextInt();
+
+        System.out.println("Введите число B:");
+        System.out.println("Число B = ");
+
+        int chislo_B = scanner.nextInt();
+
+        System.out.println("Какую операцию провести?");
+        System.out.println("1. Сумма");
+        System.out.println("2. Разность");
+        System.out.println("3. Умножение");
+        System.out.println("4. Частное");
+        System.out.println("5. Выйти");
+
+        while (true) {
+            int command = scanner.nextInt();
+            switch (command) {
+                case 1:
+                    System.out.println("A + B = " + (chislo_A + chislo_B));
+                    break;
+                case 2:
+                    System.out.println("A - B = " + (chislo_A - chislo_B));
+                    break;
+                case 3:
+                    System.out.println("A * B = " + (chislo_A * chislo_B));
+                    break;
+                case 4:
+                    System.out.println("A / B = " + (double) (chislo_A / chislo_B));
+                    break;
+                case 5:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Команда не верна!!!");
             }
-        }
-        for (int i = N; i > 1; i--) {
-            System.out.print(array[i] + " ");
         }
     }
 }
+
